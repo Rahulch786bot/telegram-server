ca191433ab9f3e9a0783b1fd32968fca2a846084
Rahulch786bot <rahulchidambaram786@gmail.com>
2026-06-10 16:57:55 +0530

    Implement CORS support in server.js
    Added CORS support to handle cross-origin requests.

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express();
 
 app.use(express.json());
 
+// CORS SUPPORT
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 const TOKEN = process.env.BOT_TOKEN;
 
 let users = [];
